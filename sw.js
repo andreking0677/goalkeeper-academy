ed25cfec2d8cd1a5469282ba3058310f4dfad0d6
Andreking067 <hernan.dagnino@autonomadeica.edu.pe>
2026-09-08 19:08:27 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-const CACHE_NAME = "tc-academy-v1";
+const CACHE_NAME = "tc-academy-v2";
 const URLS = [
+  "./",
   "./index.html",
   "./manifest.json",
   "./icon.svg",
-  "https://unpkg.com/react@18/umd/react.production.min.js",
-  "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
-  "https://unpkg.com/@babel/standalone/babel.min.js",
+  "./tailwind.css",
+  "./react.production.min.js",
+  "./react-dom.production.min.js",
+  "./babel.min.js",
 ];
 
 self.addEventListener("install", (event) => {
